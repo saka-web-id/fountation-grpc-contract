e80ac9a0b18cca3f4a911cfb5730dccc336aeb62
sakawijaya <sakawijaya@gmail.com>
2026-02-27 14:35:26 +0700

    Initial gRPC contract

---
+++ src/main/java/id/web/saka/fountation/FountationGrpcContractApplication.java
@@ -0,0 +1,13 @@
+package id.web.saka.fountation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FountationGrpcContractApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FountationGrpcContractApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/web/saka/fountation/grpc/FountationGrpcContractApplicationTests.java
@@ -0,0 +1,13 @@
+package id.web.saka.fountation.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FountationGrpcContractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
